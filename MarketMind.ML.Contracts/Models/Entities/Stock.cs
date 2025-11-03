3c2db62d9b9088b7dd0b9df0d149944bdadcf3d2
Anish Gyawali <anish.gyawali7@gmail.com>
2025-11-03 10:37:32 -0600

    Initial project multi-layer API, Contracts, Trainer

---
+++ MarketMind.ML.Contracts/Models/Entities/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketMind.ML.Contracts.Models.Entities
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public long Volume { get; set; }
+        public string? Sentiment { get; set; } 
+    }
+}
---
+++ MarketMindAI.API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MarketMind.ML.Contracts.Models.Entities;
+
+namespace MarketMind.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Stock> Stocks => Set<Stock>();
+    }
+}
